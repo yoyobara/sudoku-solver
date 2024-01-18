107fc521776da87c40126de5ccca6c48f75c8873
yoyobara <yotambarak6@gmail.com>
2024-01-18 23:47:32 +0200

    figured react out ig

---
+++ frontend/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <p>hello world!</p>
+  );
+}
+
+export default App;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::fs::{FileServer, relative};
+
+#[macro_use] extern crate rocket;
+
+#[rocket::launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", FileServer::from(relative!("frontend/build/")))
 }
