0df4c32e0381f0cf57a258e95a31c217c6cb5582
yoyobara <yotambarak6@gmail.com>
2024-01-25 23:06:42 +0200

    solve function. require testing

--- src/board.rs
+++ src/board.rs
@@ -1,5 +1,4 @@
-use std::{ops::RangeBounds, array, vec};
-
+use std::array;
 use serde::Deserialize;
 
 const EMPTY: i8 = 0;
@@ -14,6 +13,14 @@ impl Board {
     fn index_ok(row: usize, column: usize) -> bool {
         let rng = 1..=9;
         rng.contains(&row) && rng.contains(&column)
+    }
+
+    /* 
+     * consumes an Iterator and checks 
+     * whether a value appears only once in it 
+     */
+    fn appears_once<T: PartialEq>(it: impl Iterator<Item = T>, val: T) -> bool {
+        it.filter(|x| *x == val).count() == 1
     }
 
     fn get(&self, row: usize, column: usize) -> Option<&i8> {
@@ -60,7 +67,35 @@ impl Board {
         empty_spots
     }
 
-    pub fn solve(&self) {
+    pub fn solve(&mut self) -> bool {
         let empty_spots = self.get_empty_spots();
+        let mut empty_index: usize = 0;
+
+        loop {
+
+            let (srow, scol) = empty_spots[empty_index];
+            let val = self.get_mut(srow, scol).unwrap();
+
+            if *val == 9 {
+                *val = 0;
+                if empty_index == 0 {
+                    return false;
+                }
+            }
+            else {
+                *val += 1;
+                let val = self.get(srow, scol).unwrap();
+
+                if Self::appears_once(self.get_box(srow, scol).iter(), val) && 
+                   Self::appears_once(self.get_row(srow).iter(), val) && 
+                   Self::appears_once(self.get_column(scol).iter(), val){
+
+                       empty_index += 1;
+                       if empty_index == empty_spots.len() {
+                           return true;
+                       }
+                }
+            }
+        }
     }
 }
