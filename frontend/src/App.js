16e98ba900dd80ffdedb0a1dfe72275f8c704692
yoyobara <yotambarak6@gmail.com>
2024-01-29 15:00:38 +0200

    wow. now the solved cells are marked!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import {useState} from "react";
 import styles from "./App.module.css"
 
-function Cell({board, setBoard, i, j}) {
+function Cell({board, setBoard, i, j, boardAutoCompleted}) {
     let idx = i * 9 + j;
     return (
         <input
@@ -11,18 +11,21 @@ function Cell({board, setBoard, i, j}) {
             value={board[idx]}
             onChange={(e) => {setBoard(board.map((v, i) => i === idx ? e.target.value : v))}}
             onFocus={(e) => e.target.select()}
+            style={{
+                color: boardAutoCompleted[idx] ? 'blue' : 'black'
+            }}
         />
     );
 }
 
-function Board({board, setBoard}) {
+function Board({board, setBoard, boardAutoCompleted}) {
     let arr = Array.from(
         { length: 9 },
         (_, i) =>
             <div className={styles.Row}>
             {Array.from(
                 { length: 9 },
-                (_, j) => <Cell key={i.toString() + j.toString()} i={i} j={j} board={board} setBoard={setBoard}/>
+                (_, j) => <Cell key={i.toString() + j.toString()} i={i} j={j} board={board} setBoard={setBoard} boardAutoCompleted={boardAutoCompleted}/>
             )}
             </div>
     );
@@ -45,7 +48,13 @@ function SolveButton({solveClick}) {
 function App() {
     let [board, setBoard] = useState(Array(81).fill(""));
 
+    // an array of 81 booleans for each cell if that cell has been auto completed or not.
+    let [boardAutoCompleted, setBoardAutoCompleted] = useState(Array(81).fill(false))
+
     let solveClick = () => {
+
+        // mark
+        setBoardAutoCompleted(Array.from({length: 81}, (_, i) => board[i] == ""))
 
         fetch("/api/solve", {
             method: "POST", 
@@ -63,7 +72,7 @@ function App() {
 
     return (
         <div className={styles.App}>
-            <Board board={board} setBoard={setBoard}/>
+            <Board board={board} setBoard={setBoard} boardAutoCompleted={boardAutoCompleted}/>
             <SolveButton solveClick={solveClick}/>
         </div>
     );
